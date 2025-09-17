7e3b4eed0360fc4fa954192cc5f4c60927da6623
jhoandryA <jhoandryoficial@gmail.com>
2025-09-17 00:10:32 -0500

    comit- UsuarioRepository

---
+++ src/main/java/com/sise/postulape_backend/usuario/domain/repositiry/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.sise.postulape_backend.usuario.domain.repositiry;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.sise.postulape_backend.usuario.domain.entities.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+}
+
