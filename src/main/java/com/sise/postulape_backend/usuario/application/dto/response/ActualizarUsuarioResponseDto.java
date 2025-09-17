1d0224614238db5788fdc77f48237b21c4a3f549
jhoandryA <jhoandryoficial@gmail.com>
2025-09-17 00:16:13 -0500

    comit- response usuario

---
+++ src/main/java/com/sise/postulape_backend/usuario/application/dto/response/ActualizarUsuarioResponseDto.java
@@ -0,0 +1,17 @@
+package com.sise.postulape_backend.usuario.application.dto.response;
+
+import lombok.Data;
+import java.sql.Timestamp;
+
+@Data
+public class ActualizarUsuarioResponseDto {
+    private Integer idUsuario;
+    private String usuario;
+    private String clave;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String nombre;
+    private Integer idPerfil;
+    private String idEliminado;
+    private Timestamp fechaCreacion;
+}
---
+++ src/main/java/com/sise/postulape_backend/usuario/application/dto/response/EliminarUsuarioResponseDto.java
@@ -0,0 +1,9 @@
+package com.sise.postulape_backend.usuario.application.dto.response;
+
+import lombok.Data;
+
+@Data
+public class EliminarUsuarioResponseDto {
+    private Integer idUsuario;
+    private String mensaje;
+}
