bbc2513fa114f0c480ab9fb345c41a76b3ba0320
jcastroroj <luisjcr.1801@gmail.com>
2025-09-16 23:11:52 -0500

    Create ActualizarAreaRequestDto

--- src/main/java/com/sise/postulape_backend/area/application/dto/request/ActualizarAreaRequestDto.java
+++ src/main/java/com/sise/postulape_backend/area/application/dto/request/ActualizarAreaRequestDto.java
@@ -1,5 +1,9 @@
 package com.sise.postulape_backend.area.application.dto.request;
 
+import lombok.Data;
+
+@Data
 public class ActualizarAreaRequestDto {
-    
+    private String nombre;
+    private String descripcion;
 }
