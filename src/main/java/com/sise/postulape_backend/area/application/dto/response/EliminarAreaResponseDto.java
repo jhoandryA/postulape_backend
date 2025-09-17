da840a73fd065a8528c0cda9ff77feaf65c0a0d0
jcastroroj <luisjcr.1801@gmail.com>
2025-09-16 23:14:40 -0500

    Create response crud

---
+++ src/main/java/com/sise/postulape_backend/area/application/dto/response/EliminarAreaResponseDto.java
@@ -0,0 +1,9 @@
+package com.sise.postulape_backend.area.application.dto.response;
+
+import lombok.Data;
+
+@Data
+public class EliminarAreaResponseDto {
+    private String mensaje;
+    private Integer idArea;
+}
---
+++ src/main/java/com/sise/postulape_backend/area/application/dto/response/InsertarAreaResponseDto.java
@@ -0,0 +1,13 @@
+package com.sise.postulape_backend.area.application.dto.response;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class InsertarAreaResponseDto {
+    private Integer idArea;
+    private String nombre;
+    private String descripcion;
+    private String idEliminado;
+    private LocalDateTime fechaCreacion;
+}
