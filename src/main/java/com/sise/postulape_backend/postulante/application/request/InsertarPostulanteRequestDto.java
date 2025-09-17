84227ad6b962124ede4ab32f801a7c6b9b652ecf
jhoandryA <jhoandryoficial@gmail.com>
2025-09-16 23:52:58 -0500

    comit-request postulante

---
+++ src/main/java/com/sise/postulape_backend/postulante/application/request/InsertarPostulanteRequestDto.java
@@ -0,0 +1,18 @@
+package com.sise.postulape_backend.postulante.application.request;
+
+import lombok.Data;
+
+@Data
+public class InsertarPostulanteRequestDto {
+    private Integer idUsuario;
+    private Integer idTipoDocumento;
+    private String numeroDocumento;
+    private Integer idGenero;
+    private Integer idEstadoCivil;
+    private String direccion;
+    private String idDepartamento;
+    private String idProvincia;
+    private String idDistrito;
+    private String telefono;
+    private String celular;
+}
